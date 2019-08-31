1b416eb4c5f620f9b5632691e20fc94b512553bb
Nanda Oktavera <codeharuka.yusa@gmail.com>
2019-09-01 08:49:00 +0900

    refactor functions param for easy usage
    Signed-off-by: Nanda Oktavera <codeharuka.yusa@gmail.com>

--- goodix_fod_wakeup.c
+++ goodix_fod_wakeup.c
@@ -16,15 +16,16 @@
 #include <stdlib.h>
 
 #define EVDEV   "/dev/input/event2" // goodix_ts input event path
+#define BLDEV   "/sys/class/backlight/panel0-backlight/bl_power"
 #define INP_OFF 325 // when screen is off
 #define DELAY   10000
 
-int send_input(uint16_t type, uint16_t code, uint16_t value)
+int send_input(char *input, uint16_t type, uint16_t code, uint16_t value)
 {
 	int fd, ret = 1;
 	struct input_event ev;
 
-	fd = open(EVDEV, O_RDWR | O_NONBLOCK);
+	fd = open(input, O_RDWR | O_NONBLOCK);
 	if (fd < 0)
 		goto out;
 
@@ -41,23 +42,21 @@ out:
 	return ret;
 }
 
-int panel_status()
+int readfint(char *file)
 {
 	int fd, ret = -1;
 	char buf[8];
-	char *bl_device = "/sys/class/backlight/panel0-backlight/bl_power";
 	ssize_t len;
 
-	fd = open(bl_device, O_RDONLY);
-
+	fd = open(file, O_RDONLY);
 	if (fd < 0)
 		goto out;
 
 	len = read(fd, buf, sizeof(buf) - 1);
-	if (len < 0)
+	if (len <= 0)
 		goto out;
 
-	buf[len] = 0;
+	buf[len - 1] = 0;
 	sscanf(buf, "%d", &ret);
 
 out:
@@ -67,30 +66,30 @@ out:
 
 void wakeup_nezuko()
 {
-	if (panel_status() <= FB_BLANK_NORMAL) // screen is already on
+	if (readfint(BLDEV) <= FB_BLANK_NORMAL) // screen is already on
 		return;
 
 	printf(":: Wake-up the screen\n");
 
-	send_input(EV_KEY, KEY_WAKEUP, 1);
-	send_input(EV_SYN, SYN_REPORT, 0);
+	send_input(EVDEV, EV_KEY, KEY_WAKEUP, 1);
+	send_input(EVDEV, EV_SYN, SYN_REPORT, 0);
 
-	send_input(EV_KEY, KEY_WAKEUP, 0);
-	send_input(EV_SYN, SYN_REPORT, 0);
+	send_input(EVDEV, EV_KEY, KEY_WAKEUP, 0);
+	send_input(EVDEV, EV_SYN, SYN_REPORT, 0);
 
-	if (panel_status() == FB_BLANK_UNBLANK)
+	if (readfint(BLDEV) == FB_BLANK_UNBLANK)
 		printf(":: Screen on.\n");
 
 	printf(":: Emulating touches\n");
 
-	send_input(EV_KEY, BTN_TOUCH, 1);
-	send_input(EV_SYN, SYN_REPORT, 0);
+	send_input(EVDEV, EV_KEY, BTN_TOUCH, 1);
+	send_input(EVDEV, EV_SYN, SYN_REPORT, 0);
 
-	send_input(EV_ABS, ABS_MT_TRACKING_ID, 2060);
-	send_input(EV_SYN, SYN_REPORT, 0);
+	send_input(EVDEV, EV_ABS, ABS_MT_TRACKING_ID, 2060);
+	send_input(EVDEV, EV_SYN, SYN_REPORT, 0);
 
-	send_input(EV_KEY, BTN_TOUCH, 0);
-	send_input(EV_SYN, SYN_REPORT, 0);
+	send_input(EVDEV, EV_KEY, BTN_TOUCH, 0);
+	send_input(EVDEV, EV_SYN, SYN_REPORT, 0);
 
 	printf(":: Done\n");
 }
@@ -117,4 +116,3 @@ int main()
 	close(fd);
 	return ret;
 }
-
