94f778e560476a5eea2ee757e90c6627f52fe402
Nanda Oktavera <codeharuka.yusa@gmail.com>
2019-08-31 11:01:24 +0900

    read screen state before wakeup the screen
    There's a case where goodix report the touches
    as INP_OFF, but the actual screen is already on,
    such as Always On Display.
    
    So, better to check the state via backlight
    interface to avoid redundant wakeups.
    Also, remove unnecessary variables and codes.
    
    Signed-off-by: Nanda Oktavera <codeharuka.yusa@gmail.com>

--- goodix_fod_wakeup.c
+++ goodix_fod_wakeup.c
@@ -1,34 +1,32 @@
-// SPDX-License-Identifier: BSD-3-Clause
-/*
+/* SPDX-License-Identifier: BSD-3-Clause
+ *
  * Goodix FOD Wakeup
  *
  * Copyright (c) 2019 Nanda Oktavera
  * Released under the terms of 3-clause BSD License
  *
  */
+
 #include <stdio.h>
 #include <string.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <linux/input.h>
+#include <linux/fb.h>
 #include <stdlib.h>
 
 #define EVDEV   "/dev/input/event2" // goodix_ts input event path
 #define INP_OFF 325 // when screen is off
-#define INP_ON  358 // when screen is on
 #define DELAY   10000
 
 int send_input(uint16_t type, uint16_t code, uint16_t value)
 {
-	int fd, ret = 0;
+	int fd, ret = 1;
 	struct input_event ev;
 
 	fd = open(EVDEV, O_RDWR);
-	if (fd < 0) {
-		printf(":: Failed to access %s\n", EVDEV);
-		ret = 1;
+	if (fd < 0)
 		goto out;
-	}
 
 	ev.type = type;
 	ev.code = code;
@@ -41,15 +39,47 @@ out:
 	return ret;
 }
 
+int panel_status()
+{
+	int fd, ret = -1;
+	char buf[8];
+	char *bl_device = "/sys/class/backlight/panel0-backlight/bl_power";
+	ssize_t len;
+
+	fd = open(bl_device, O_RDONLY);
+
+	if (fd < 0)
+		goto out;
+
+	len = read(fd, buf, sizeof(buf) - 1);
+	if (len < 0)
+		goto out;
+
+	buf[len] = 0;
+	sscanf(buf, "%d", &ret);
+
+out:
+	close(fd);
+	return ret;
+}
+
 void wakeup_nezuko()
 {
-	printf(":: Wake-up the screen...\n");
+	if (panel_status() <= FB_BLANK_NORMAL) // screen is already on
+		return;
+
+	printf(":: Wake-up the screen\n");
+
 	send_input(EV_KEY, KEY_WAKEUP, 1);
 	send_input(EV_SYN, SYN_REPORT, 0);
+
 	usleep(DELAY);
+
 	send_input(EV_KEY, KEY_WAKEUP, 0);
 	send_input(EV_SYN, SYN_REPORT, 0);
-	printf(":: Screen on.\n");
+
+	if (panel_status() == FB_BLANK_UNBLANK)
+		printf(":: Screen on.\n");
 }
 
 
@@ -63,6 +93,7 @@ int main()
 
 	printf(":: Reading %s\n", EVDEV);
 	fd = open(EVDEV, O_RDONLY);
+
 	while(read(fd, &ev, evsize)) {
 		if (ev.code == INP_OFF && ev.value == 1)
 			wakeup_nezuko();
